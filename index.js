3acb3b1174085494b030e33ddb84cd4e7a5974fa
PRANAVSONAR23 <pranavsonar2311@gmail.com>
2025-01-04 18:02:38 +0530

    cors

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({  
   credentials: true,
-  origin: 'http://localhost:5173'
+  origin: '*'
 }));
 
 async function getUserDataFromRequest(req) {
